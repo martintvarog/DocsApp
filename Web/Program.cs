8b63d2e07f56f45d22c530b8fd5e7e987f51a49b
Tvarog <m_tvarog@utb.cz>
2023-02-07 20:19:14 +0100

    add message pack support

--- Web/Program.cs
+++ Web/Program.cs
@@ -1,10 +1,19 @@
 using Di;
+using MessagePack;
+using MessagePack.AspNetCoreMvcFormatter;
+using MessagePack.Resolvers;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 WebDi.ConfigureServices(builder.Services, builder.Configuration);
-builder.Services.AddControllers().AddXmlDataContractSerializerFormatters();
+builder.Services.AddControllers().AddXmlDataContractSerializerFormatters().AddMvcOptions(options =>
+{
+    options.OutputFormatters.Add(
+        new MessagePackOutputFormatter(
+            MessagePackSerializerOptions.Standard.WithResolver(ContractlessStandardResolver.Instance)));
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
